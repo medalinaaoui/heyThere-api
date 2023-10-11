c4e64063def97f20768c57dad74af1bdfe3f03d7
Med Ali Naaoui <chayga00@gmail.com>
2023-10-11 10:23:06 +0100

    updates in db connection

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   "https://hey-there-api-fc5a5cd59152.herokuapp.com",
   "http://localhost:5173",
-  "http://localhost:8800",
+  "https://hey-there-api-fc5a5cd59152.herokuapp.com/",
 ];
 
 export default allowedOrigins;
