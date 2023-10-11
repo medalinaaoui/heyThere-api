c647b5755a1aecd1eea2984c6d3861b2fde10060
Med Ali Naaoui <chayga00@gmail.com>
2023-10-11 08:23:49 +0100

    updates on db connection

--- model/db.js
+++ model/db.js
@@ -2,8 +2,8 @@ import mysql from "mysql2";
 import "dotenv/config";
 
 export const db = mysql.createConnection({
-  host: "phtfaw4p6a970uc0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "l97hboax73u8p2x3",
-  password: "n2r3ro6vdrrkfrpq",
-  database: "c9wqnxnjqxmyby8r",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
